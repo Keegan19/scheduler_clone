aa7dcc750d7c5298e9f8abb800c39898a698c8f8
Keegan19 <killenkeegan@hotmail.com>
2017-02-27 19:12:45 -0500

    Review
    Bug Fixes in priority queue and removing finished threads

---
+++ Project1.cpp
@@ -0,0 +1,35 @@
+// Project1.cpp : Defines the entry point for the console application.
+//
+
+#include "myscheduler.h"
+#include <list>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  int numberOfCPUs = 12;
+  MyScheduler scheduler = MyScheduler(STRFwoP, numberOfCPUs);
+	srand(time(NULL));
+  int totalThreads = 75;
+  
+//  for (int i = 0; i < totalThreads; i++)
+//  {
+//    scheduler.CreateThread(i, (rand() % 20) + 1, priority, i);
+//  }
+  
+  for (int i = 0; i < totalThreads; i++)
+	{
+		scheduler.CreateThread(i + rand() % 5, 1 + rand() % 40, rand() % 20, i);
+	}
+
+	scheduler.PrintThreads("Threads", scheduler.Threads);
+
+	scheduler.Go();
+  
+	return 0;
+}
+
+
